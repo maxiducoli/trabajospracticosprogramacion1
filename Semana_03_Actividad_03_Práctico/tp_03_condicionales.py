59f4655a4a81bed16a9d974ada5f01940a376b0c
maxiducoli <maxiducoli@gmail.com>
2025-03-31 21:40:47 -0300

    Agrego TP 03 - Estructuras condicionales ejercicio 7

--- Semana_03_Actividad_03_Práctico/tp_03_condicionales.py
+++ Semana_03_Actividad_03_Práctico/tp_03_condicionales.py
@@ -1,3 +1,4 @@
+from statistics import median, mode, mean
 #1) Escribir un programa que solicite la edad del usuario.
 # Si el usuario es mayor de 18 años, deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”. 
 
@@ -73,4 +74,38 @@ if len(password) >= 8 and len(password) <= 14:
     print("Ha ingresado una contraseña correcta")
 else:
     print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+
+
+#6) El paquete statistics de python contiene funciones que permiten tomar una lista de números y calcular la moda,
+# la mediana y la media de dichos números.
+# Un ejemplo de su uso es el siguiente:
+# from statistics import mode, median, mean mi_lista = [1,2,5,5,3] mean(mi_lista)
+# En la documentación oficial se puede encontrar más información sobre este paquete:
+# https://docs.python.org/es/3.8/library/statistics.html.
+# La moda (mode), 
+# la mediana (median) 
+# la media (mean) son parámetros estadísticos que se pueden utilizar para predecir
+# la forma de una distribución normal a partir del siguiente criterio: 
+#   ● Sesgo positivo o a la derecha: cuando la media es mayor que la mediana y,
+#   a su vez, la mediana es mayor que la moda. 
+#   
+#   ● Sesgo negativo o a la izquierda: cuando la media es menor que la mediana y,
+#   a su vez, la mediana es menor que la moda.
+#   
+#   ● Sin sesgo: cuando la media, la mediana y la moda son iguales.
+#   
+#   
+#   Teniendo en cuenta lo antes mencionado, escribir un programa que tome la lista numeros_aleatorios,
+#   calcule su moda, su mediana y su media y las compare para determinar si hay sesgo positivo,
+#   negativo o no hay sesgo. Imprimir el resultado por pantalla.
+#   Definir la lista numeros_aleatorios de la siguiente forma:
+#   import random numeros_aleatorios = [random.randint(1, 100) for i in range(50)] 
+#   Nota: el bloque de código anterior crea una lista con 50 números entre 1 y 100 elegidos de forma aleatoria. 
+
+import random 
+numeros_aleatorios = [random.randint(1, 100) for i in range(50)]
+
+print(mean(numeros_aleatorios))
+print(median(numeros_aleatorios))
+print(mode(numeros_aleatorios))
 
