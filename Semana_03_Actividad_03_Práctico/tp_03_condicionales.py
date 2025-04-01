e6d53d7873184d10e772eed2f5b908ec744d47e3
maxiducoli <maxiducoli@gmail.com>
2025-03-31 21:02:04 -0300

    Agrego TP 03 Condicionales

---
+++ Semana_03_Actividad_03_Práctico/tp_03_condicionales.py
@@ -0,0 +1,13 @@
+#1) Escribir un programa que solicite la edad del usuario.
+# Si el usuario es mayor de 18 años, deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”. 
+
+edad = input("Ingrese su edad: ")
+if edad.isnumeric():
+    edad = int(edad)
+    if edad >= 18:
+        print("Es mayor de edad")
+    else:
+        print("Es menor de edad")
+else:
+    print("Por favor, ingrese un número válido para la edad.")
+
